abc2b1a04baa858e0dad6fef014e03970d427968
Maxence Tourniayre <maxence.tourniayre@gmail.com>
2024-10-16 15:25:41 +0200

    feat: add info message

--- src/express/server.ts
+++ src/express/server.ts
@@ -13,9 +13,12 @@ app.use(express.json());
 app.post('/api/notification', async (req, res) => {
   const notification = req.body as Notification;
 
+  console.log('Received notification', notification);
+
   const authorization = req.headers.authorization;
   if (!authorization) {
     res.status(401).send('Missing Authorization header');
+    console.log('Received notification without Authorization header');
     return;
   }
 
@@ -25,6 +28,7 @@ app.post('/api/notification', async (req, res) => {
 
   if (!payload) {
     res.status(401).send('Unauthorized');
+    console.log('Received notification with invalid token');
     return;
   }
 
@@ -32,11 +36,13 @@ app.post('/api/notification', async (req, res) => {
 
   if (username != notification.username) {
     res.status(401).send('Unauthorized');
+    console.log('Received notification with invalid username');
     return;
   }
 
   if (!notification.discordId) {
     res.status(400).send('Missing discordId');
+    console.log('Received notification without discordId');
     return;
   }
 
@@ -47,6 +53,6 @@ app.post('/api/notification', async (req, res) => {
 
 export function startServer() {
   app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+    console.log(`Server started on port ${port} 🚀`);
   });
 }
